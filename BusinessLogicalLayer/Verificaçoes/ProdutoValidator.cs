981b129328c68696fa6225589fd05123d6dc61ce
Nicolettigf <gfnicoletti@gmail.com>
2022-06-16 22:33:57 -0300

    produto validadorProdutoValidador

--- BusinessLogicalLayer/Verificaçoes/ProdutoValidator.cs
+++ BusinessLogicalLayer/Verificaçoes/ProdutoValidator.cs
@@ -8,5 +8,44 @@ namespace BusinessLogicalLayer.Verificaçoes
 {
     public class ProdutoValidator
     {
+        public string ValidateProdutoNome(string nome)
+        {
+            if (String.IsNullOrWhiteSpace(nome))
+            {
+                return "string nao pode ser vazia";
+            }
+            //exemplo de xy usa dois caracteres
+            if (nome.Length <= 2)
+            {
+                return "nome insuficiente";
+            }
+            return "";
+
+        }
+        public string ValidateDescrisaoProduto(string descrisao)
+        {
+            if (String.IsNullOrWhiteSpace(descrisao))
+            {
+                return "descrisao nao informada";
+            }
+            if (descrisao.Length <= 2)
+            {
+                return "descrisao insuficiente";
+            }
+            return "";
+
+        }
+        public string ValidateLaboratorio(string laboratorio)
+        {
+            if (!String.IsNullOrWhiteSpace(laboratorio))
+            {
+                return "laboratorio nao informado";
+            }
+            return "";
+
+        }
+
+
     }
+}
 }

5f7129f035286175c5cb74165ef11a7c572d41f0
Rubens Mette Junior <rubensmettejunior@gmail.com>
2022-07-05 14:11:26 -0300

    hasheando senha

--- BusinessLogicalLayer/BusinessLL/FuncionarioBll.cs
+++ BusinessLogicalLayer/BusinessLL/FuncionarioBll.cs
@@ -19,20 +19,28 @@ namespace BusinessLogicalLayer.BusinessLL
                 using (TransactionScope scope = new TransactionScope())
                 {
                     response = enderecoDAL.Insert(funcionario.Endereco);
-                    response = funcionarioDAL.Insert(funcionario);
                     if (!response.HasSuccess)
                     {
                         return response;
                     }
+
+                    funcionario.Senha = HashSenha.ComputeSha256Hash(funcionario.Senha);
+                    response = funcionarioDAL.Insert(funcionario);
+
+                    if (!response.HasSuccess)
+                    {
+                        return response;
+                    }
+
                     scope.Complete();
-                }//scope.Dispose();
+                }
             }
             return response;
         }
 
-        public Response Update(Funcionario item)
+        public Response Update(Funcionario funcionario)
         {
-            Response response = FuncionarioValidator.Validate(item);
+            Response response = FuncionarioValidator.Validate(funcionario);
             if (response.HasSuccess)
             {
                 {
@@ -40,14 +48,22 @@ namespace BusinessLogicalLayer.BusinessLL
                     {
                         EnderecoDAL enderecoDAL = new EnderecoDAL();
 
-                        response = enderecoDAL.Update(item.Endereco);
-                        response = funcionarioDAL.Update(item);
+                        response = enderecoDAL.Update(funcionario.Endereco);
+                        funcionario.Senha = HashSenha.ComputeSha256Hash(funcionario.Senha);
+
+                        if (!response.HasSuccess)
+                        {
+                            return response;
+                        }
+
+                        response = funcionarioDAL.Update(funcionario);
+
                         if (!response.HasSuccess)
                         {
                             return response;
                         }
                         scope.Complete();
-                    }//scope.Dispose();
+                    }
                 }
                 return response;
             }
@@ -74,14 +90,14 @@ namespace BusinessLogicalLayer.BusinessLL
 
         }
 
-        public Response LoginBLL(string email,string senha)
+        public Response LoginBLL(string email, string senha)
         {
             SingleResponse<Funcionario> response = FuncionarioDAL.LoginDAL(email);
-            if(response.Item.Ativo == false)
+            if (response.Item.Ativo == false)
             {
                 return new Response("Funcionario Desabilitado", false);
             }
-            if (response.Item.Senha == senha)
+            if (response.Item.Senha == HashSenha.ComputeSha256Hash(senha))
             {
                 SystemParameters.Logar(response.Item);
                 return new Response("login Certo", true);
---
+++ DataAccessLayer/HashSenha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class HashSenha
+    {
+        public static string ComputeSha256Hash(string senha)
+        {
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Adicionando "sal" à senha para mais segurança ao hashear
+                senha = "jd82b!*¨$aAOI23" + senha + "lLiD4¬?2";
+
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
